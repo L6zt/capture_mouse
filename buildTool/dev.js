624e3d4a5e9fd4f5164bfd8621dae029f9d4874b
jiaozhen <jiaozhen@liepin.com>
2019-02-01 21:58:50 +0800

    初始化自动打包编译配置 --- 第一次提交

---
+++ buildTool/dev.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  entry: path.resolve(__dirname, '../src/index.js'),
+  output: {
+  	path: path.resolve(__dirname, '../dist'),
+	  filename: 'captrue_mouse.js',
+	  library: 'captrue_mouse',
+  },
+  module: {
+	  rules: [{
+		  test: /\.js$/,
+		  exclude: /node_modules/,
+		  use: ['babel-loader', 'eslint-loader'],
+	  }],
+  },
+  devtool: 'source-map',
+  devServer: {
+  	contentBase: path.resolve(__dirname, '../dist'),
+  	compress: false,
+	  historyApiFallback: false,
+	  hot: true,
+	  port: 7001
+  },
+  plugins: [
+  	new CleanWebpackPlugin([path.resolve(__dirname, '../dist')]),
+  	new webpack.HotModuleReplacementPlugin(),
+	  new HtmlWebpackPlugin({
+		  template: path.resolve(__dirname, '../src/index.html')
+	  })
+  ]
+}
---
+++ src/index.js
@@ -0,0 +1,2 @@
+const log = '我要输出日志..';
+console.log(log);
